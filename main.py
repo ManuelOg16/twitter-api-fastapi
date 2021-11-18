d085f917d85248de4d13da27631b5c34e2db7f19
manuel <vigemi2@gmail.com>
2021-11-18 16:31:19 -0500

    users_signup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Python
+import json  #libreria que me sirve para trabajar con archivos .json
 from uuid import UUID
 from datetime import date, datetime
 from typing import Optional, List  # de la liberaria de Typing puedo importar la clase List esta me permite definir el tipo de una variable va aser una lista de cosas
@@ -12,7 +13,7 @@ from pydantic import Field
 #FastAPI
 from fastapi import FastAPI
 from fastapi import status
-
+from fastapi import Body
 
 app = FastAPI()
 
@@ -22,21 +23,20 @@ class UserBase(BaseModel): # have the information basic the user the return in R
     user_id: UUID = Field(...)  #UUID =Universally Unique IDentifier es una clase especial de python que nos permite colocar un identificador unico cada vez a cada uno de lso usuarios  y entidades que nosotros creamos en nuestra aplicación
     email: EmailStr = Field(...)
 
-class PasswordMixin(BaseModel):   # Creamos este nuevo modelo para no repetir codigo del password
-    password: str = Field(
-    ...,
-        min_length=8,
-        max_length=64,
-        example='password'
-    )
-
-class UserLogin(PasswordMixin,UserBase): #The model return when the user  Logged in our API
+# class PasswordMixin(BaseModel):   # Creamos este nuevo modelo para no repetir codigo del password
     # password: str = Field(
     # ...,
     #     min_length=8,
-    #     max_length=64
+    #     max_length=64,
+    #     example='password'
     # )
-    pass
+
+class UserLogin(UserBase): #The model return when the user  Logged in our API
+    password: str = Field(
+    ...,
+        min_length=8,
+        max_length=64
+    )
 
 class User(UserBase): # others tasks with users we used a User, but not the password
     first_name: str = Field(
@@ -51,7 +51,7 @@ class User(UserBase): # others tasks with users we used a User, but not the pass
     )
     birth_date: Optional[date] = Field(default=None)
 
-class UserRegister(PasswordMixin,User): #Nuevo modelo para el registro del usuario en la aplicación
+class UserRegister(UserLogin,User): #Nuevo modelo para el registro del usuario en la aplicación
     # password: str = Field(
     # ...,
     #     min_length=8,
@@ -84,7 +84,7 @@ class Tweet(BaseModel):
     summary= "Register a User",
     tags=["Users"] # la path operation va estar dentro de la pestaña Users
 )
-def signup():
+def signup(user: UserRegister = Body(...)):
     """
     Signup
 
@@ -99,8 +99,19 @@ def signup():
         - email: Emailstr
         - first_name: str
         - last_name: str
-        - birth_date: str
+        - birth_date: date
     """
+    with open("users.json", "r+", encoding= "utf-8") as f:  #r+ para leer y escribir
+        results = json.loads(f.read()) #como lo hizo el profe la funcion loads del modulo json carga un string y lo transforma en un simil a json en este caso en un diccioario por lo tanto results va tener un diccionario en este caso va ser una lista de diccionarios lo que va contener results
+        
+        #Ahora vamos a convertir el body que nos viene a nosotros en un diccionario tambien, recordemos que la funcion signup necesita un Request body , necesita un user que venga desde el cliente para poder registrar el usuario , la comunicacion es el cliente le envia la info de registro al servidor el servidor la toma la guarda  retorna que el usuario se creo correctamente para lograr eso colocamos en los parametros de la funcion signup ese request body
+        user_dict= user.dict() #cadaa uno de los request body que nos viene como parametro tiene tambien el metodo dict este es un metodo interno que le asigna fastAPI al Request body para poder transformar el json en diccionario de manera explicita
+        user_dict["user_id"] = str(user_dict["user_id"])   #cambiar dos parametros que tengo dentro de este diccionario
+        user_dict["birth_date"] = str(user_dict["birth_date"]) #los datos UUID , date no se pueden convertir a json de manera natural, lo tengo que hacer manualmente  con un casting str()            
+        results.append(user_dict) #hago un append dentro d emi variable results de ese diccionario le añado el nuevo usuario a mi json con el user_dict
+        f.seek(0)        #voy a mover al principio de mi archivo por que acabo de escribir algo, entonces para no tener error y moverme lo hago con seek en el primer byte (0) para no emepzar a escribir listas, entonces con seek me muevo al byte cero es decir al incio del archivo y a partir de ahi escribo lo que debo escibir
+        f.write(json.dumps(results))  #ahora voy a escribir en el archivo, lo debo convertir la lista de diccionarios en un json con dumps
+        return user  #del user que me viene como parametro para decirle al usuario que se creo correctamente
 
 ### Login a user
 @app.post(
