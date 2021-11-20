052418a89f8676a86996706b86bd4ba0b0786303
manuel <vigemi2@gmail.com>
2021-11-20 16:04:38 -0500

    users_show_all

--- main.py
+++ main.py
@@ -133,8 +133,23 @@ def login():
     tags=["Users"] 
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
 
+    Parameters:
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: date
+    
+    Returns a json list with all users in the app, with the following Keys:
+
+    
+    """
+    with open("users.json", "r", encoding= "utf-8") as f:
+        results = json.loads(f.read()) # cargamos todo el contenido del archivo en la variable results
+        return results #como results es un simil a json es una lista de diccionarios podemos retornarlo por que fastapi puede convertir esto automaticamente a un json
 ### Show a User
 @app.get(
     path= "/users/{user_id}",
